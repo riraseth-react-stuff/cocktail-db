670643c67013e657850f8d55e76a2bb00150e038
Łukasz <riraseth@gmail.com>
2020-02-29 18:04:11 +0100

    Update CocktailList.js

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,5 +1,24 @@
 import React from 'react';
-
-export default function CocktailList({ cocktails }) {
-  return <h1>cocktailList component</h1>;
+import Cocktail from './Cocktail';
+export default function CocktailList({ cocktails, loading }) {
+  if (loading) {
+    return <h2 className="section-title">Loading...</h2>;
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">
+        no cocktails matched your search criteria
+      </h2>
+    );
+  }
+  return (
+    <section className="section">
+      <h2 className="section-title">cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map(cocktail => {
+          return <Cocktail key={cocktail.id} {...cocktail}></Cocktail>;
+        })}
+      </div>
+    </section>
+  );
 }
