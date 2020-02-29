309f3d03bf411c09923282786541591dcffffe75
Łukasz <riraseth@gmail.com>
2020-02-29 17:51:24 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,11 +8,42 @@ export default function Home() {
   const [cocktails, setCocktails] = useState([]);
 
   useEffect(() => {
-    fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
-    )
-      .then(response => response.json())
-      .then(data => setCocktails(data.drinks));
+    setLoading(true);
+    async function getDrinks() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+        );
+        const data = await response.json();
+        const { drinks } = data;
+        if (drinks) {
+          const newCocktails = drinks.map(drink => {
+            const {
+              idDrink,
+              strDrink,
+              strDrinkThumbnail,
+              strAlcoholic,
+              strGlass
+            } = drink;
+
+            return {
+              id: idDrink,
+              name: strDrink,
+              image: strDrinkThumbnail,
+              info: strAlcoholic,
+              glass: strGlass
+            };
+          });
+          setCocktails(newCocktails);
+        } else {
+          setCocktails([]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    getDrinks();
   }, [searchTerm]);
   return (
     <main>
